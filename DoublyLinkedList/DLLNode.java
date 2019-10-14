6349e4c4a657d232b04c1e1bacdb43b945648b8b
meghana gottapu <meghanagottapu@gmail.com>
2019-10-14 23:48:14 +0530

    Update DLLNode.java

--- DoublyLinkedList/DLLNode.java
+++ DoublyLinkedList/DLLNode.java
@@ -1,15 +1,9 @@
-public class DoublyLinkedLTest {
-
-	public static void main(String[] args) {
-		DoublyLinkedL dll=new DoublyLinkedL(10);
-		System.out.print("Doubly linked list is: ");
-		dll.insert(20);
-		dll.insertAtPos(1,30);
-		dll.insertFirst(40);
-		dll.display();
-		dll.delete(20);
-		System.out.println();
-		dll.display();
+public class DLLNode {
+	int data;
+	DLLNode prev;
+	DLLNode next;
+	DLLNode(){data=0;}
+	DLLNode(int d){
+		data=d;
 	}
-
 }
